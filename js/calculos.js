7d5b43c4967c3ef5a88d3503e68eeab6e377bfb6
Miguel Ribeiro <42856@ufp.edu.pt>
2021-12-04 19:02:20 +0000

    enganei-me no ultimo commit meti as função binario no campo decimal mas agr esta certo

--- js/calculos.js
+++ js/calculos.js
@@ -1,3 +1,16 @@
+//*******************************Decimal******************************************************// 
+//esta funcão decimal n é correta so estou a testar isto é copy paste da binaria
+function calcdecimal () {
+    var num = document.getElementById("leitord").value;
+    let binary = (num % 2).toString();
+    for (; num > 1; ){
+        num = parseInt(num / 2);
+        binary =  (num % 2) + (binary);
+    }
+    document.getElementById("binario").innerHTML = "Resultado: " + binary;
+}
+
+//*******************************Binário******************************************************// 
 function calcbinario () {
     var num = document.getElementById("leitorb").value;
     let binary = (num % 2).toString();
